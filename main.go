2ba1a9a594092e69b4a7403df365fa00deab050c
amaurysoares <amauryrodrigo@gmail.com>
2021-04-21 13:23:48 -0300

    adicionadas chamadas ao pedidosControler

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/farofadev/goesic/controllers"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -18,8 +19,32 @@ func Hello(res http.ResponseWriter, req *http.Request, param httprouter.Params) 
 
 func main() {
     router := httprouter.New()
+
+	pedidosController := &controllers.PedidosController{}
+
+	http.HandleFunc("/api/pedidos", pedidosController.Index)
+	// http.HandleFunc("/api/pedidos/criar", pedidosController.Criar)
+	// http.HandleFunc("/api/pedidos/exibir", pedidosController.Exibir)
+
     router.GET("/", Index)
     router.GET("/hello/:name", Hello)
 
     log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+
+	
+
+
+// func get(http, caminho, metodo) {
+// 	http.HandleFunc(caminho, func (response, request) {
+// 		if (request.method != "GET") {
+// 			response.WriteHeader(http.StatusNotFound)
+// 			response.Write([]byte(`Page not found. (Method not allowed)`))
+// 			return
+// 		}
+
+// 		metodo()
+// 	})
+// }
+
