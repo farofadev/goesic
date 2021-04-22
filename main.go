0fdfd43f30d5b952411c12a5fb48fcf2b6f12a0b
Vagner do Carmo <vluzrmos@gmail.com>
2021-04-22 14:59:36 -0300

    check database connection and reconnect

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/farofadev/goesic/controllers"
 	"github.com/farofadev/goesic/lib"
@@ -20,7 +21,7 @@ func main() {
 
 	log.Println("Iniciando...")
 
-	lib.DBConnectAndSetDefault();
+	db := lib.DBConnectAndSetDefault();
 
     router := httprouter.New()
 
@@ -32,6 +33,23 @@ func main() {
 	router.GET("/pedidos/:id", pedidosController.Show)
 
 	defer lib.GetDefaultDBConnection().Close()
+
+	go func() {
+		for {
+			log.Println("Verificando banco de dados")
+
+			_, err := db.Query("SHOW TABLES;")
+
+			if err != nil {
+				log.Println("Conexão falhou, tentando reconectar...")
+
+				lib.DBConnectAndSetDefault();
+			}
+
+			time.Sleep(30 * time.Second)
+		}
+	}()
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
